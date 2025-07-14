7681f8a68f194b87ddb191dc9e9c4c525403e238
SpyCoderX <151940429+SpyCoderX@users.noreply.github.com>
2025-07-13 23:28:47 -0400

    CORS policy enabled

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 
+
 const express = require('express');
 const fetch = require('node-fetch');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post('/api/servers', async (req, res) => {
