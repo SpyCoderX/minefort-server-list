40924a1df95d164d48148f9dc4e9c9e2da3ebde7
SpyCoderX <151940429+SpyCoderX@users.noreply.github.com>
2025-07-18 18:50:05 -0400

    Background

--- onload.js
+++ onload.js
@@ -225,8 +225,8 @@ async function minefortOnLoad(serverListElement, aboutElement) {
               box-shadow:
                 0 4px 16px 0 rgba(0,0,0,0.18),
                 0 0 0 1px rgba(255,255,255,0.08),
-                inset 0 0 8px 8px rgba(0,0,0,0.2),
-                inset 0 0 16px 24px rgba(0,0,0,0.1);
+                inset 0 0 8px 8px rgba(0,0,0,0.1),
+                inset 0 0 16px 24px rgba(0,0,0,0.05);
             }
             .${uniqueId}:hover {
               background: ${planColorsHover[planTier]};
